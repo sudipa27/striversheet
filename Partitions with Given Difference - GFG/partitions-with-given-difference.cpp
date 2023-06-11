df852e2a87af048b1ec7cef7d004443073ea2220
Sudipa Bhattacharjee <91149507+sudipa27@users.noreply.github.com>
2023-06-11 16:00:26 +0530

    Added solution - LeetHub

---
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+int mod =(int)1e9+7;
+
+int findWays(vector<int> &num, int tar){
+     int n = num.size();
+
+    vector<vector<int>> dp(n,vector<int>(tar+1,0));
+    
+    if(num[0] == 0) dp[0][0] =2;  // 2 cases -pick and not pick
+    else dp[0][0] = 1;  // 1 case - not pick
+    
+    if(num[0]!=0 && num[0]<=tar) dp[0][num[0]] = 1;  // 1 case -pick
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 0; target<=tar; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken)%mod;
+        }
+    }
+    return dp[n-1][tar];
+}
+
+int countPartitions(int n, int d, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<n;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d <0 || (totSum-d)%2 ) return 0;
+    
+    return findWays(arr,(totSum-d)/2);
+}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, d;
+        cin >> n >> d;
+        vector<int> arr;
+
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            arr.push_back(x);
+        }
+
+        Solution obj;
+        cout << obj.countPartitions(n, d, arr) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
